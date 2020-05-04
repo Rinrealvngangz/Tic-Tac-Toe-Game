a555eb1897c508b93c9817d8c5e3ca4f8375d76a
Tuan <Rinrealvngangz.com>
2020-05-04 09:40:50 +0700

    Add project game tic-tac-toe basic

---
+++ Tic-tac-toe/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.gofirst = new System.Windows.Forms.Button();
+            this.p1 = new System.Windows.Forms.Label();
+            this.p2 = new System.Windows.Forms.Label();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn1.Enabled = false;
+            this.btn1.ForeColor = System.Drawing.Color.Transparent;
+            this.btn1.Location = new System.Drawing.Point(84, 36);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(55, 47);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn2.Enabled = false;
+            this.btn2.ForeColor = System.Drawing.Color.White;
+            this.btn2.Location = new System.Drawing.Point(135, 79);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(51, 46);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn.Enabled = false;
+            this.btn.ForeColor = System.Drawing.Color.White;
+            this.btn.Location = new System.Drawing.Point(135, 36);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(51, 47);
+            this.btn.TabIndex = 2;
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn4.Enabled = false;
+            this.btn4.ForeColor = System.Drawing.Color.White;
+            this.btn4.Location = new System.Drawing.Point(84, 79);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(55, 46);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn5.Enabled = false;
+            this.btn5.ForeColor = System.Drawing.Color.White;
+            this.btn5.Location = new System.Drawing.Point(182, 36);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(51, 47);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn6.Enabled = false;
+            this.btn6.ForeColor = System.Drawing.Color.White;
+            this.btn6.Location = new System.Drawing.Point(182, 79);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(51, 46);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn7.Enabled = false;
+            this.btn7.ForeColor = System.Drawing.Color.White;
+            this.btn7.Location = new System.Drawing.Point(84, 122);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(55, 45);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn8.Enabled = false;
+            this.btn8.ForeColor = System.Drawing.Color.White;
+            this.btn8.Location = new System.Drawing.Point(135, 122);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(51, 45);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btn9.Enabled = false;
+            this.btn9.ForeColor = System.Drawing.Color.White;
+            this.btn9.Location = new System.Drawing.Point(182, 122);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(51, 45);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // gofirst
+            // 
+            this.gofirst.BackColor = System.Drawing.Color.Chartreuse;
+            this.gofirst.Location = new System.Drawing.Point(292, 36);
+            this.gofirst.Name = "gofirst";
+            this.gofirst.Size = new System.Drawing.Size(75, 41);
+            this.gofirst.TabIndex = 9;
+            this.gofirst.Text = "Go";
+            this.gofirst.UseVisualStyleBackColor = false;
+            this.gofirst.Click += new System.EventHandler(this.gofirst_Click_1);
+            // 
+            // p1
+            // 
+            this.p1.AutoSize = true;
+            this.p1.Location = new System.Drawing.Point(308, 108);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(56, 17);
+            this.p1.TabIndex = 10;
+            this.p1.Text = "Player1";
+            // 
+            // p2
+            // 
+            this.p2.AutoSize = true;
+            this.p2.Location = new System.Drawing.Point(308, 162);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(56, 17);
+            this.p2.TabIndex = 11;
+            this.p2.Text = "Player2";
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.BackColor = System.Drawing.Color.SlateBlue;
+            this.btnPlayAgain.Location = new System.Drawing.Point(84, 190);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(62, 40);
+            this.btnPlayAgain.TabIndex = 12;
+            this.btnPlayAgain.Text = "Again";
+            this.btnPlayAgain.UseVisualStyleBackColor = false;
+            this.btnPlayAgain.Click += new System.EventHandler(this.btnPlayAgain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ClientSize = new System.Drawing.Size(464, 278);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.gofirst);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button gofirst;
+        private System.Windows.Forms.Label p1;
+        private System.Windows.Forms.Label p2;
+        private System.Windows.Forms.Button btnPlayAgain;
+    }
+}
+
---
+++ Tic-tac-toe/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string play;
+        private void gofirst_Click_1(object sender, EventArgs e)
+        {
+            checkPlayer();
+            btn.Enabled = true;
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+            btn9.Enabled = true;
+        }
+
+        public void checkPlayer()
+        {
+            Random random = new Random();
+            int num = random.Next(2);
+            bool[] mangKT = { true, false };
+            bool p = mangKT[num];
+            if (p == true)
+            {
+                MessageBox.Show("Player1 Go First");
+            }
+            else
+                MessageBox.Show("Player2 Go First");
+
+            if (p == true)
+            {
+                play = "X";
+            }
+            else
+                play = "O";
+        }
+        public void checkGo()
+        {
+            if (play == "X")
+            {
+                play = "O";
+
+                p1.ForeColor = Color.Blue;
+                if (p1.ForeColor == Color.Blue)
+                {
+                    p2.ForeColor = Color.Black;
+                };
+            }
+            else {
+                play = "X";
+                p2.ForeColor = Color.Red;
+                if (p2.ForeColor == Color.Red)
+                {
+                    p1.ForeColor = Color.Black;
+                };
+            }
+           
+        }
+
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            btn.Text = play;
+            checkGo();
+            btn.Enabled = false;
+            checkWin();
+           
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btn1.Text = play;
+            checkGo();
+            btn1.Enabled = false;
+            checkWin();
+        }
+        private void btn5_Click(object sender, EventArgs e)
+        {
+              
+            btn5.Text = play;
+            checkGo();
+            btn5.Enabled = false;
+            checkWin();
+           
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+                
+            btn4.Text = play;
+            checkGo();
+            btn4.Enabled = false;
+            checkWin();
+           
+      
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+                  
+            btn2.Text = play;
+            checkGo();
+            btn2.Enabled = false;
+            checkWin();
+         
+  
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+          
+           
+            btn6.Text = play;
+            checkGo();
+            btn6.Enabled = false;
+            checkWin();
+      
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {    
+            btn7.Text = play;
+            checkGo();
+            btn7.Enabled = false;
+            checkWin();
+         
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+               
+           btn8.Text = play;
+            checkGo();
+            btn8.Enabled = false;
+            checkWin();
+        
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {                 
+            btn9.Text = play;
+            checkGo();
+            btn9.Enabled = false;
+            checkWin();
+          
+        }
+        public void check_Who_Win()
+        {
+            if (play == "X")
+            {
+                play = "O";
+                MessageBox.Show("Player 2 Win");
+            }
+            else if (play == "O")
+            {
+                play = "X";
+                MessageBox.Show("Player 1 Win");
+            }
+            
+
+        }
+        public void playAgain()
+        {
+            btn.Text = "";
+            btn1.Text = "";
+            btn2.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+            p2.ForeColor = Color.Black;
+            p1.ForeColor = Color.Black;
+
+        }
+        public void checkWin()
+        {
+                    
+             
+            if ((btn.Text == "X") && (btn5.Text == "X") && (btn1.Text == "X") || (btn.Text == "O") && (btn5.Text == "O") && (btn1.Text == "O"))
+
+                check_Who_Win();
+
+
+            else if ((btn1.Text == "X") && (btn4.Text == "X") && (btn7.Text == "X") || (btn7.Text == "O") && (btn4.Text == "O") && (btn1.Text == "O"))
+                check_Who_Win();
+
+            else if ((btn5.Text == "X") && (btn6.Text == "X") && (btn9.Text == "X") || (btn5.Text == "O") && (btn6.Text == "O") && (btn9.Text == "O"))
+
+                check_Who_Win();
+
+            else if ((btn7.Text == "X") && (btn8.Text == "X") && (btn9.Text == "X") || (btn7.Text == "O") && (btn8.Text == "O") && (btn9.Text == "O"))
+
+                check_Who_Win();
+
+            else if ((btn1.Text == "X") && (btn2.Text == "X") && (btn9.Text == "X") || (btn1.Text == "O") && (btn2.Text == "O") && (btn9.Text == "O"))
+
+                check_Who_Win();
+
+            else if ((btn2.Text == "X") && (btn5.Text == "X") && (btn7.Text == "X") || (btn2.Text == "O") && (btn5.Text == "O") && (btn7.Text == "O"))
+
+                check_Who_Win();
+
+            else if ((btn4.Text == "X") && (btn2.Text == "X") && (btn6.Text == "X") || (btn4.Text == "O") && (btn2.Text == "O") && (btn6.Text == "O"))
+
+                check_Who_Win();
+
+            else if ((btn.Text == "X") && (btn2.Text == "X") && (btn8.Text == "X") || (btn.Text == "O") && (btn2.Text == "O") && (btn8.Text == "O"))
+
+                check_Who_Win();
+            else if ((btn.Enabled == false) && (btn1.Enabled == false) && (btn2.Enabled == false) && (btn4.Enabled == false) && (btn5.Enabled == false) && (btn6.Enabled == false) && (btn7.Enabled == false) && (btn8.Enabled == false) && (btn9.Enabled == false))
+            {
+                MessageBox.Show("Hoa!!!");
+            }
+
+        }
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            playAgain();
+          
+        }
+    }
+
+     
+    }
+
